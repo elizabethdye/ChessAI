62b8303177e7824765a87213df7d39832be308fa
Mitchell Sharp <mitchellsha@gmail.com>
2015-09-16 16:46:08 -0500

    Revert "Revert "AlphaBeta thingssss""
    This reverts commit 5951c22e651319472d5cc2638047cd7862d5f1c9.

--- src/chess/ai/AlphaBeta.java
+++ src/chess/ai/AlphaBeta.java
@@ -4,20 +4,24 @@ import chess.core.Chessboard;
 import chess.core.Move;
 
 public class AlphaBeta extends Searcher {
-	
+	private int alpha;
+	private int beta;
 	@Override
 	public MoveScore findBestMove(Chessboard board, BoardEval eval, int depth) {
 		setup(board, eval, depth);
-		MoveScore result = evalMoves(board, eval, depth);
+		alpha = Integer.MIN_VALUE;
+		beta = Integer.MAX_VALUE;
+		boolean isMax = true;
+		MoveScore result = evalMoves(board, eval, depth, isMax);
 		tearDown();
 		return result;
 	}
 	
-	MoveScore evalMoves(Chessboard board, BoardEval eval, int depth) {
+	MoveScore evalMoves(Chessboard board, BoardEval eval, int depth, boolean isMax) {
 		MoveScore best = null;
 		for (Move m: board.getLegalMoves()) {
 			Chessboard next = generate(board, m);
-			MoveScore result = new MoveScore(-evalBoard(next, eval, depth - 1), m);
+			MoveScore result = new MoveScore(-evalBoard(next, eval, depth - 1, !isMax), m);
 			if (best == null || result.getScore() > best.getScore()) {
 				best = result;
 			}
@@ -25,15 +29,22 @@ public class AlphaBeta extends Searcher {
 		return best;
 	}	
 	
-	int evalBoard(Chessboard board, BoardEval eval, int depth) {
+	int evalBoard(Chessboard board, BoardEval eval, int depth, boolean isMax) {
 		if (!board.hasKing(board.getMoverColor()) || board.isCheckmate()) {
 			return -eval.maxValue();
 		} else if (board.isStalemate()) {
 			return 0;
-		} else if (depth == 0) {
+		} else if (depth == 0 || alpha >= beta) {
 			return evaluate(board, eval);
 		} else {
-			return evalMoves(board, eval, depth).getScore();
+			int score = evalMoves(board, eval, depth, isMax).getScore();
+			if( isMax ){
+				alpha = score;
+			}
+			else{
+				beta = score;
+			}
+			return score;
 		}
 	}
 }
