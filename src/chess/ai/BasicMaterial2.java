eaaf9cec8db14ceea23437e133595d5caa0e28cc
elizabethdye <dyeea@hendrix.edu>
2015-09-18 15:35:43 -0500

    BM2222222

--- src/chess/ai/BasicMaterial2.java
+++ src/chess/ai/BasicMaterial2.java
@@ -7,17 +7,23 @@ import chess.core.ChessPiece;
 import chess.core.Chessboard;
 
 
-public class BasicMaterial implements BoardEval {
+public class BasicMaterial2 implements BoardEval {
 	final static int MAX_VALUE = 20000;
 	private EnumMap<ChessPiece,Integer> values = new EnumMap<ChessPiece,Integer>(ChessPiece.class);
 	
-	public BasicMaterial() {
-		values.put(ChessPiece.BISHOP, 3);
-		values.put(ChessPiece.KNIGHT, 3);
-		values.put(ChessPiece.PAWN, 1);
-		values.put(ChessPiece.QUEEN, 9);
-		values.put(ChessPiece.ROOK, 5);
+	public BasicMaterial2() {
+		values.put(ChessPiece.BISHOP, 325);
+		values.put(ChessPiece.KNIGHT, 320);
+		values.put(ChessPiece.PAWN, 100);
+		values.put(ChessPiece.QUEEN, 975);
+		values.put(ChessPiece.ROOK, 500);
 		values.put(ChessPiece.KING, MAX_VALUE);
+		//values.put(ChessPiece.BISHOP, 3);
+		//values.put(ChessPiece.KNIGHT, 3);
+		//values.put(ChessPiece.PAWN, 1);
+		//values.put(ChessPiece.QUEEN, 9);
+		//values.put(ChessPiece.ROOK, 5);
+		//values.put(ChessPiece.KING, MAX_VALUE);
 	}
 
 	@Override
@@ -49,3 +55,4 @@ public class BasicMaterial implements BoardEval {
 		return values.get(piece);
 	}
 }
+
